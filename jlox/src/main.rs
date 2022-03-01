cd3563dc1cd149d764d3fcd4e717296375055eba
Charles Dunbar <ccdunbar@gmail.com>
2022-02-28 23:33:20 -0600

    Initial functions for jlox

--- jlox/src/main.rs
+++ jlox/src/main.rs
@@ -1,3 +1,40 @@
+use std::{env, fs, io, io::Write};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    //print!("{}", args.len());
+    match args.len() {
+        1 => run_prompt(),
+        2 => run_file(&args[1]),
+        _ => {
+            println!("Usage: jlox [script]");
+            std::process::exit(64);
+        }
+    }
 }
+
+fn run_file(cmd: &str) {
+    let source = fs::read_to_string(cmd).expect("Got an error reading file");
+    run(&source);
+}
+
+fn run_prompt() {
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+
+        let bytes = io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to parse input!");
+
+        // Catch Ctrl-D
+        if bytes == 0 {
+            break;
+        }
+        print!("{}", input);
+    }
+}
+
+fn run(source: &str) {}
