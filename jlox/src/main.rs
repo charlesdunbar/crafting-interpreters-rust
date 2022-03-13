685e83261081db1a28b0bd6963e403b1521cd536
Charles Dunbar <ccdunbar@gmail.com>
2022-03-13 14:29:47 -0500

    Refactor to Lox struct with impl

--- jlox/src/main.rs
+++ jlox/src/main.rs
@@ -1,12 +1,13 @@
 use std::{env, fs, io, io::Write};
 
 fn main() {
+    let lox = Lox { hadError: false };
     let args: Vec<String> = env::args().collect();
 
     //print!("{}", args.len());
     match args.len() {
-        1 => run_prompt(),
-        2 => run_file(&args[1]),
+        1 => lox.run_prompt(),
+        2 => lox.run_file(&args[1]),
         _ => {
             println!("Usage: jlox [script]");
             std::process::exit(64);
@@ -14,27 +15,40 @@ fn main() {
     }
 }
 
-fn run_file(cmd: &str) {
-    let source = fs::read_to_string(cmd).expect("Got an error reading file");
-    run(&source);
+struct Lox {
+    hadError: bool,
 }
 
-fn run_prompt() {
-    loop {
-        print!("> ");
-        io::stdout().flush().unwrap();
-        let mut input = String::new();
-
-        let bytes = io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to parse input!");
-
-        // Catch Ctrl-D
-        if bytes == 0 {
-            break;
+impl Lox {
+    fn run_file(&self, cmd: &str) {
+        let source = fs::read_to_string(cmd).expect("Got an error reading file");
+        let result = self.run(&source).expect("Error!");
+        // Exit on errors
+        if !result {
+            std::process::exit(65);
         }
-        print!("{}", input);
+    }
+
+    fn run_prompt(&self) {
+        loop {
+            print!("> ");
+            io::stdout().flush().unwrap();
+            let mut input = String::new();
+
+            let bytes = io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to parse input!");
+
+            // Catch Ctrl-D
+            if bytes == 0 {
+                break;
+            }
+            print!("{}", input);
+            self.run(&input).unwrap();
+        }
+    }
+
+    fn run(&self, source: &str) -> Result<bool, Box<dyn std::error::Error>> {
+        Ok(true)
     }
 }
-
-fn run(source: &str) {}
